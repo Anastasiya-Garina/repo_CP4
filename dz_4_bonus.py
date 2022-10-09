967cdda9bb267ed1a97359c9ab0f685142599899
Anastasiya-Garina <114314685+Anastasiya-Garina@users.noreply.github.com>
2022-10-09 20:31:36 +0300

    Add files via upload

--- dz_4_bonus.py
+++ dz_4_bonus.py
@@ -1,9 +1,9 @@
-array = [1, 2, 3, 4, 5]
+array = [1, 2, 3, 4, 5, 5, 5, 6]
 delta = 4
 minim = min(array)
 cont = 0
-for i in range(1, len(array)+1):
-    if array[i] - delta == minim:
+for i in range(0, len(array)):
+    if int(array[i]) - delta == minim:
         cont += 1
         i += 1
 print(cont)
